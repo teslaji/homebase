0eca761074da76cacbd6820f8ee07aab001047f9
robus <kbhatt365@gmail.com>
2017-02-16 08:48:55 +0530

    Added Template

--- metaphor/views.py
+++ metaphor/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+#from django.http import Http404
+#from django.template import loader
 from django.http import HttpResponse
 
 def home (request):
-        return HttpResponse ("Hello, World")
+        return render(request, "main/home.html",{'message':'Companies House UK'})
